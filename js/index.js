884dd43821a7d4217fe5e51b2591a6db27a9e419
Your Name Here <chelapsys@gmail.com>
2014-05-06 19:32:07 -0400

    nydeets1 div fix

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
         /* Check the location of each desired element */
         $('.nv_mainsection').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var middle_of_object = $(this).position().top + ($(this).outerHeight() / 2);
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
+            /* If the middle of the object is at the bottom of window, fade it it */
+            if( bottom_of_window > middle_of_object ){
                 
                 $(this).animate({'opacity':'1'},800);
                     
